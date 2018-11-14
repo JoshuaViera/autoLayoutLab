cc1ece48855c8d6d96d9db82ecb0ecfc273407c9
JoshuaViera <joshuaviera95@gmail.com>
2018-11-14 14:34:47 -0500

    Initial commit - Auto Layout Lab

---
+++ autoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Joshua Viera on 11/14/18.
+//  Copyright © 2018 Joshua Viera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
